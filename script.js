0b6d388f1fcf4c213b9eaf2bddecd34bcb36b491
Roviosda <Roviosda@gmail.com>
2025-04-16 21:42:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -777,11 +777,21 @@
         addFooter(doc);
         // Сохранение PDF
         doc.save(`Результаты теста жизнестойкости - ${userData.firstName} ${userData.lastName}.pdf`);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////// 
+        const pdfBlob = pdf.output('blob');
+
+
+
+          
+       //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////   
+
+        
         // Отправка данных на сервер
-        await sendDataToServer(userData);
+        await sendDataToServer(userData, pdfBlob));
       } catch (error) {
         console.error('Ошибка при генерации PDF:', error);
-        alert('Произошла ошибка при создании отчета. Пожалуйста, попробуйте еще раз.');
+        //alert('Произошла ошибка при создании отчета. Пожалуйста, попробуйте еще раз.');
       }
     }
     /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -838,7 +848,7 @@
       doc.text(splitRec, x, y);
     }
     // Обновленная функция отправки данных
-    async function sendDataToServer(userData) {  
+    async function sendDataToServer(userData, pdfBlob) {  
       try {
      // Создаем FormData для отправки
     const formData = new FormData();
@@ -847,15 +857,13 @@
     formData.append('email', userData.email);
     formData.append('phone', userData.phone || '');
     formData.append('testResults', JSON.stringify(scores));
+    formData.append('pdfFile', pdfBlob, 'test_results.pdf');
+          
           
  const response = await fetch('http://212.67.11.10:3000/api/submit', {
       method: 'POST',
       body: formData
     });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
 
     const result = await response.json();
     console.log('Данные успешно отправлены на сервер:', result);
